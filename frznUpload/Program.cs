3251c2921746ddc52a4c359045be29b336858364
fritzen <finn-blumenstock@web.de>
2018-10-31 22:33:56 +0100

    Add project files.

---
+++ frznUpload/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace frznUpload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
