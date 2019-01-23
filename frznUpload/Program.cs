6396621c79217170bd9bc7862154e2f891a032ac
fritzen <finn-blumenstock@web.de>
2019-01-23 16:06:13 +0100

    minor changes

--- frznUpload/Program.cs
+++ frznUpload/Program.cs
@@ -20,6 +20,9 @@ namespace frznUpload.Server
 
         static void Main(string[] args)
         {
+            for (int i = 0; i < args.Length; i++)
+                Console.WriteLine(args[i]);
+
             Console.WriteLine("Starting Logger");
 
             Logger.Open();
